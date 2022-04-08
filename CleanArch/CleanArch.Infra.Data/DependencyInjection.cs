74e63b0818cfe157faf612dd09d35ecae671dd37
PedroNovaes25 <dodroxpedro28@gmail.com>
2022-04-08 18:06:42 -0300

    Estrutura Clean architecture montada com app web

---
+++ CleanArch/CleanArch.Infra.Data/DependencyInjection.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArch.Infra.Data
+{
+    public static class DependencyInjection
+    {
+
+        public static IServiceCollection InfrastructureAdd(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+              options.UseSqlServer(
+                   configuration.GetConnectionString("DefaultConnection")));
+            services.AddDatabaseDeveloperPageExceptionFilter();
+
+            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                .AddEntityFrameworkStores<ApplicationDbContext>();
+
+            return services;
+        }
+
+    }
+}
